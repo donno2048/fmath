722ab16b1a6a7383f880e9b25083a7b4443a9118
donno2048 <just4now666666@gmail.com>
2024-07-30 06:55:26 +0000

    add log2

--- pyfmath.c
+++ pyfmath.c
@@ -15,11 +15,13 @@ _declare(PyFloat_FromLong, PyFloat_FromDouble(*(double*)&x), const long long x);
 declare(abs, parsed & INT64_MAX, Float);
 declare(sign, ~parsed >> 0x3F, Bool);
 declare(sqrt, (parsed >> 1) + (one >> 1), Float);
+_declare(_log2, PyFloat_FromDouble((double)(parsed >> 0x17) - 126.4), const PyObject * self, const PyObject * args);
 _declare(_pow, PyFloat_FromLong((parse(args[0]) - one) * PyFloat_AS_DOUBLE(args[1]) + one), const PyObject * self, const PyObject *const *args, const Py_ssize_t nargs);
 static PyMethodDef fmath_methods[] = {
     {add_name(pow), METH_FASTCALL, "pow: return x to the power of y"},
     {add_name(abs), METH_O, "abs: return the absolute value of x"},
     {add_name(sign), METH_O, "sign: return True if x>=0 else False"},
+    {add_name(log2), METH_O, "log2: return the log base 2 of x"},
     {add_name(sqrt), METH_O, "sqrt: return the square root of x"},
     {NULL, NULL, 0, NULL}
 };
