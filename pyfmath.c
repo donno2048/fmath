38ba92a6d05582945c3b7d46edaa6daca68993a1
Elisha Hollander <just4now666666@gmail.com>
2024-08-05 20:39:19 +0300

    refactor `log2`

--- pyfmath.c
+++ pyfmath.c
@@ -8,14 +8,14 @@ extern "C" {
 #define parse(arg) (*(long long*)_parse(PyFloat_AS_DOUBLE(arg)))
 #define parsed parse(args)
 #define _declare(func, value, ...) const static inline PyObject * func(__VA_ARGS__) {return value;}
-#define declare(func, operation, type) _declare(_##func, Py##type##_FromLong(operation), const PyObject * self, const PyObject * args)
+#define declare(func, operation, typeA, typeB) _declare(_##func, Py##typeA##_From##typeB(operation), const PyObject * self, const PyObject * args)
 const long long one = 0x3FF0000000000000;
 const static inline void * _parse(const double arg){return & arg;}
 _declare(PyFloat_FromLong, PyFloat_FromDouble(*(double*)&x), const long long x);
-declare(abs, parsed & INT64_MAX, Float);
-declare(sign, ~parsed >> 0x3F, Bool);
-declare(sqrt, (parsed >> 1) + (one >> 1), Float);
-_declare(_log2, PyFloat_FromDouble((double)(parsed >> 0x17) - 126.4), const PyObject * self, const PyObject * args);
+declare(abs, parsed & INT64_MAX, Float, Long);
+declare(sign, ~parsed >> 0x3F, Bool, Long);
+declare(sqrt, (parsed >> 1) + (one >> 1), Float, Long);
+declare(log2, (double)(parsed >> 0x34) - 1023, Float, Double);
 _declare(_pow, PyFloat_FromLong((parse(args[0]) - one) * PyFloat_AS_DOUBLE(args[1]) + one), const PyObject * self, const PyObject *const *args, const Py_ssize_t nargs);
 const static inline PyObject *_min(const PyObject * self, const PyObject *const *args, const Py_ssize_t nargs) {
     long long m = parse(args[0]), cur = parse(args[nargs - 1]);
